8d570d2c224b7d8e72eee5cdc05ffe3364f16d33
tsub <tsubasatakayama511@gmail.com>
2017-08-08 21:35:48 +0900

    Fix error when select current branch

--- functions/fzf_git_recent_branch.fish
+++ functions/fzf_git_recent_branch.fish
@@ -2,7 +2,7 @@ function fzf_git_recent_branch -d "Efficient fish keybinding for fzf with git br
   eval "git branch --sort=-authordate | __fzfcmd --query (commandline)" | read -z select
 
   if not test -z $select
-    eval "git checkout (builtin string trim "$select")"
+    eval "git checkout (builtin string trim --left --chars='* ' '$select')"
   end
 
   commandline -f repaint

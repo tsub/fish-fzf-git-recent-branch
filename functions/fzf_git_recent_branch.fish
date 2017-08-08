f2ee3e89f73f1a3effb49660f977aa603fee12cd
Tsubasa Takayama <t-takayama@feedforce.jp>
2017-08-08 15:17:17 +0900

    Implement fzf_git_recent_branch function

--- functions/fzf_git_recent_branch.fish
+++ functions/fzf_git_recent_branch.fish
@@ -1,2 +1,9 @@
 function fzf_git_recent_branch -d "Efficient fish keybinding for fzf with git branch"
+  eval "git branch --sort=-authordate | __fzfcmd --query (commandline)" | read -z select
+
+  if not test -z $select
+    eval "git checkout (builtin string trim "$select")"
+  end
+
+  commandline -f repaint
 end

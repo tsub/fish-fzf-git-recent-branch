e379486ddc54ef8fc9d18292d86d23447446234b
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-12 22:47:32 +0900

    Avoid error in __fzfcmd function

--- functions/fzf_git_recent_branch.fish
+++ functions/fzf_git_recent_branch.fish
@@ -1,5 +1,5 @@
 function fzf_git_recent_branch -d "Efficient fish keybinding for fzf with git branch"
-  eval "git branch --sort=-authordate | __fzfcmd --query (commandline)" | read -z select
+  eval "git branch --sort=-authordate | fzf --query (commandline)" | read -z select
 
   if not test -z $select
     eval "git checkout (builtin string trim --left --chars='* ' '$select')"

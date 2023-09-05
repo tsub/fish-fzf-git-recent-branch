e2d5a6205ffd476cdd00a89f4b1742d651c6304e
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-09-05 11:24:26 +0900

    Fix an error of empty string

--- functions/fzf_git_recent_branch.fish
+++ functions/fzf_git_recent_branch.fish
@@ -1,8 +1,9 @@
 function fzf_git_recent_branch -d "Efficient fish keybinding for fzf with git branch"
   eval "git branch --sort=-authordate | fzf --query (commandline)" | read -z select
 
-  if not test -z $select
-    eval "git checkout (builtin string trim --left --chars='* ' '$select')"
+  if test -n $select
+    set -l branch (builtin string trim --left --chars='* ' "$select")
+    eval "git checkout $branch"
   end
 
   commandline -f repaint
